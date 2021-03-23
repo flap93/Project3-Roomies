d16bf75d4d30664f606b65c604a41149cf6dbc06
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2021-03-23 19:55:51 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express =  require('express');
+//const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+
+//connectDB();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+//app.use('/api/users', require('./routes/api/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
