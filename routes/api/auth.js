af6256f704f7459b7a863cac92a18d39213a6a54
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2021-04-07 20:51:09 -0400

    deploying4

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -18,7 +18,7 @@ router.get('/', auth, async (req, res) => {
       res.json(user);
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
+      res.send('Server Error');
     }
   });
 
@@ -33,7 +33,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.json({ errors: errors.array() });
     }
 
     const { email, password } = req.body;
@@ -43,7 +43,6 @@ router.post(
 
       if (!user) {
         return res
-          .status(400)
           .json({ errors: [{ msg: 'Invalid Credentials' }] });
       }
 
@@ -51,7 +50,6 @@ router.post(
 
       if (!isMatch) {
         return res
-          .status(400)
           .json({ errors: [{ msg: 'Invalid Credentials' }] });
       }
 
@@ -72,7 +70,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server error');
+      res.send('Server error');
     }
   }
 );
